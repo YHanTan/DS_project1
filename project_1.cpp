ef2688a267d172805203e963a05e3e7d1ab26cc5
YHanTan <56245065+YHanTan@users.noreply.github.com>
2019-10-11 23:52:52 +0800

    input&output

--- project_1.cpp
+++ project_1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <string>
+#include <istream>
+#include <cstring>
+#include <stdlib.h>
+#include <fstream>
+#define MAX 1001
+
 using namespace std;
 
 // Pieces definition
@@ -149,188 +155,217 @@ int Pieces [19 /*kind */ ][4 /* ROW */ ][4 /* COL */] =
 
 int main()
 {
+    ifstream input;
+    ofstream output;
+    input.open("Tetris.data");
+    output.open("Tetris.output");
     int row, col;
     string type;
     int type_num;
     int drop_pos;
-
-    cin >> row >> col;
-    cin >> type >> drop_pos;
-    int game[row+4][col+8]={0};
-    for(int i=0; i<row+4; i++){
-        for(int j=0; j<col+8; j++){
-            game[i][j]=0;
-        }
-    }
-    int col_first[col+8]={row+4};
-
-    while(type!="End"){
-        int block_now[4][4] = {0};
-        if(type=="T1"){
-            type_num = 0;
-        }
-        else if(type=="T2"){
-            type_num = 1;
-        }
-        else if(type=="T3"){
-            type_num = 2;
-        }
-        else if(type=="T4"){
-            type_num = 3;
-        }
-        else if(type=="L1"){
-            type_num = 4;
-        }
-        else if(type=="L2"){
-            type_num = 5;
-        }
-        else if(type=="L3"){
-            type_num = 6;
-        }
-        else if(type=="L4"){
-            type_num = 7;
-        }
-        else if(type=="J1"){
-            type_num = 8;
-        }
-        else if(type=="J2"){
-            type_num = 9;
-        }
-        else if(type=="J3"){
-            type_num = 10;
-        }
-        else if(type=="J4"){
-            type_num = 11;
-        }
-        else if(type=="S1"){
-            type_num = 12;
-        }
-        else if(type=="S2"){
-            type_num = 13;
-        }
-        else if(type=="Z1"){
-            type_num = 14;
-        }
-        else if(type=="Z2"){
-            type_num = 15;
-        }
-        else if(type=="I1"){
-            type_num = 16;
-        }
-        else if(type=="I2"){
-            type_num = 17;
-        }
-        else if(type=="O"){
-            type_num = 18;
-        }
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                block_now[i][j] = Pieces[type_num][i][j];
+    string line;
+    string command[MAX];
+    int index=0;
+    if(input.is_open()){
+        input >> row >> col;
+        //cout << "reading data...\n";
+        cout << row << ' ' << col << '\n';
+        int game[row+4][col+8]={0};
+            for(int i=0; i<row+4; i++){
+                for(int j=0; j<col+8; j++){
+                    game[i][j]=0;
+                }
             }
-        }
+        while(!input.eof()){
+            input >> type;
+            cout << type << ' ';
+            if(type!="End"){
+                input >> drop_pos;
+                cout << drop_pos << '\n';
+            }
+            else break;
 
-        int flag=0, can=0;
-        int place_row;
-        for(int i=3; i<row+4; i++){
-            for(int row_pie=3;row_pie>=0;row_pie--){
-                int col_pie;
-                for(col_pie=0; col_pie<4; col_pie++){
-                    if(block_now[row_pie][col_pie] + game[i-(3-row_pie)][drop_pos+3+col_pie]==2){
-                        flag=1;
+            //int col_first[col+8]={row+4};
+            //while(type!="End"){
+                int block_now[4][4] = {0};
+                if(type=="T1"){
+                    type_num = 0;
+                }
+                else if(type=="T2"){
+                    type_num = 1;
+                }
+                else if(type=="T3"){
+                    type_num = 2;
+                }
+                else if(type=="T4"){
+                    type_num = 3;
+                }
+                else if(type=="L1"){
+                    type_num = 4;
+                }
+                else if(type=="L2"){
+                    type_num = 5;
+                }
+                else if(type=="L3"){
+                    type_num = 6;
+                }
+                else if(type=="L4"){
+                    type_num = 7;
+                }
+                else if(type=="J1"){
+                    type_num = 8;
+                }
+                else if(type=="J2"){
+                    type_num = 9;
+                }
+                else if(type=="J3"){
+                    type_num = 10;
+                }
+                else if(type=="J4"){
+                    type_num = 11;
+                }
+                else if(type=="S1"){
+                    type_num = 12;
+                }
+                else if(type=="S2"){
+                    type_num = 13;
+                }
+                else if(type=="Z1"){
+                    type_num = 14;
+                }
+                else if(type=="Z2"){
+                    type_num = 15;
+                }
+                else if(type=="I1"){
+                    type_num = 16;
+                }
+                else if(type=="I2"){
+                    type_num = 17;
+                }
+                else if(type=="O"){
+                    type_num = 18;
+                }
+                for(int i=0;i<4;i++){
+                    for(int j=0;j<4;j++){
+                        block_now[i][j] = Pieces[type_num][i][j];
+                    }
+                }
+
+                int flag=0, can=0;
+                int place_row;
+                for(int i=3; i<row+4; i++){
+                    for(int row_pie=3;row_pie>=0;row_pie--){
+                        int col_pie;
+                        for(col_pie=0; col_pie<4; col_pie++){
+                            if(block_now[row_pie][col_pie] + game[i-(3-row_pie)][drop_pos+3+col_pie]==2){
+                                flag=1;
+                                break;
+                            }
+                        }
+                        if(flag==1){
+                            can=1;
+                            flag=0;
+                            break;
+                        }
+                        else if(flag!=1 && row_pie==0 && col_pie==4 && i==row+3){
+                            can=2;
+                            break;
+                        }
+                    }
+                    if(can==1){
+                        can = 0;
+                        place_row = i-1;
+                        break;
+                    }
+                    else if(can==2){
+                        can = 0;
+                        place_row = i;
                         break;
                     }
                 }
-                if(flag==1){
-                    can=1;
-                    flag=0;
-                    break;
+                int tmp_prev[row+4][col+4]={0};
+                for(int i=place_row, l=3;i>=0 && l>=0;i--, l--){
+                    for(int j=drop_pos+3, k=0;j<col+8 && k<4;j++, k++){
+                        tmp_prev[i][j] = game[i][j];
+                        game[i][j] += block_now[l][k];
+                    }
                 }
-                else if(flag!=1 && row_pie==0 && col_pie==4 && i==row+3){
-                    can=2;
-                    break;
-                }
-            }
-            if(can==1){
-                can = 0;
-                place_row = i-1;
-                break;
-            }
-            else if(can==2){
-                can = 0;
-                place_row = i;
-                break;
-            }
-        }
-        int tmp_prev[row+4][col+4]={0};
-        for(int i=place_row, l=3;i>=0 && l>=0;i--, l--){
-            for(int j=drop_pos+3, k=0;j<col+8 && k<4;j++, k++){
-                tmp_prev[i][j] = game[i][j];
-                game[i][j] += block_now[l][k];
-            }
-        }
 
-        //detect & delete row
-        int count_one = 0;
-        for(int i=4;i<row+4;i++){
-            for(int j=4;j<col+4;j++){
-                if(game[i][j]==1){
-                    count_one++;
+                //detect & delete row
+                int count_one = 0;
+                for(int i=4;i<row+4;i++){
+                    for(int j=4;j<col+4;j++){
+                        if(game[i][j]==1){
+                            count_one++;
+                        }
+                        if(count_one==col){
+                            for (int row_move = i; row_move > 0; row_move--){
+                                for (int col_move = 4; col_move < col+8; col_move++){
+                                    game[row_move][col_move] = game[row_move-1][col_move];
+                                }
+                            }
+                        }
+                    }
+                        count_one = 0;
                 }
-                if(count_one==col){
-                    for (int row_move = i; row_move > 0; row_move--){
-                        for (int col_move = 4; col_move < col+8; col_move++){
-                            game[row_move][col_move] = game[row_move-1][col_move];
+
+                //end_game
+                int game_over=0;
+
+                //detect col over board
+                for(int i=0;i<col+4 && game_over==0;i++){
+                    for(int j=col+4; j<col+8; j++){
+                        if(game[i][j]==1){
+                            game_over = 2;
+                            break;
                         }
                     }
                 }
-            }
-                count_one = 0;
-        }
 
-        //end_game
-        int game_over=0;
+                //detect row over board
+                for(int i=0;i<4 && game_over==0;i++){
+                    for(int j=0; j<col+4; j++){
+                        if(game[i][j]==1){
+                            game_over = 1;
+                            break;
+                        }
+                    }
+                }
 
-        //detect col over board
-        for(int i=0;i<col+4 && game_over==0;i++){
-            for(int j=col+4; j<col+8; j++){
-                if(game[i][j]==1){
-                    game_over = 2;
+                if(game_over==2){
+                    for(int k=0;k<row+4;k++){
+                        for(int l=0;l<col+4;l++){
+                            game[k][l] = tmp_prev[k][l];
+                        }
+                    }
+                }
+                //cout << game_over;
+                if(game_over!=0){
+                    for(int i=4; i<row+4; i++){
+                        for(int j=4;j<col+4;j++){
+                            output << game[i][j];
+                        }
+                        output << endl;
+                    }
                     break;
                 }
-            }
-        }
-
-        //detect row over board
-        for(int i=0;i<4 && game_over==0;i++){
-            for(int j=0; j<col+4; j++){
-                if(game[i][j]==1){
-                    game_over = 1;
+                //cin >> type;
+                if(type=="End"){
+                    for(int i=4; i<row+4; i++){
+                        for(int j=4;j<col+4;j++){
+                            output << game[i][j];
+                        }
+                        output << endl;
+                    }
                     break;
                 }
-            }
+                //cin >> drop_pos;
+            //}
+                /*input.getline(line, 100);
+                command[index++] = line;
+            }*/
         }
-for(int i=4; i<row+4; i++){
-        for(int j=4;j<col+4;j++){
-            cout << game[i][j];
-        }
-        cout << endl;
     }
-        if(game_over==2){
-            for(int k=0;k<row+4;k++){
-                for(int l=0;l<col+4;l++){
-                    game[k][l] = tmp_prev[k][l];
-                }
-            }
-        }
-        //cout << game_over;
-        if(game_over!=0)
-            break;
-        cin >> type;
-        if(type=="End")
-            break;
-        cin >> drop_pos;
-    }
-
-
+    input.close();
     return 0;
 }
